c32f1129fa2bf01bc90f33e5042b138435114786
Michael Markieta <michael@spatialanalysis.ca>
2012-12-23 19:19:49 -0500

    function added
    BuildCircle returns coordinates for perimeter of circle geometry

---
+++ src/PolarGrid.py
@@ -0,0 +1,19 @@
+__author__ = 'michaelmarkieta'
+
+import numpy as np
+
+def BuildCircle(Rho=1, Centroid=(0,0), Tau=1000):
+    CircleGeom = []
+    UniqueID = 1
+    Precision = 360.0 / float(Tau)
+    for angle in np.arange(0, 360.001, Precision):
+        Y = np.sin(np.radians(angle))
+        X = np.cos(np.radians(angle))
+
+        AdjustedY = (Y + float(Centroid[1])) * float(Rho)
+        AdjustedX = (X + float(Centroid[0])) * float(Rho)
+
+        CircleGeom.append([UniqueID, [AdjustedX, AdjustedY]])
+        UniqueID += 1
+
+    return CircleGeom
