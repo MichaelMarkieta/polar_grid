5aec33a928e7fca264e341e7e4f47b43147d39f8
Michael Markieta <michael@spatialanalysis.ca>
2012-12-26 17:24:42 -0500

    fixed issue #1
    geometry can be created for any combination of Tau and Theta

--- PolarGrid/PolarGrid.py
+++ PolarGrid/PolarGrid.py
@@ -23,12 +23,20 @@ def PolarGrid(Rho=1, Centroid=(0,0), Theta=2, Rings=1, Tau=1000):
     CoordList = [] # Store the coordinate pairs
     PolarGeom = [] # Stores the unique id of a radial divider and it's geometric coordinate pairs
 
-    AngleTheta = 360 / int(Theta)
-    Precision = 360.0 / float(Tau)
-    UniqueID = 1
+    # Number of points along the perimeter per radial divider in relation to Tau (more points as Tau increases)
+    TauRelativeToTheta = np.ceil(float(Tau) * (1.0 / float(Theta)))
+
+    # Tau adjusted for rounding error in TauRelativeToTheta when using ceiling method
+    AdjustedTau = TauRelativeToTheta * Theta
+
+    UniqueID = 1 # The unique identifier for each radial divider
+    CountIterations = 0 # Start counter for each pass in the for loop at 0
+
+    Min = 0.0 # First angle, in degrees, on our polar grid
+    Max = 360.0 # Last angle, in degrees, on our polar grid
 
     # Iterate around a circle (360 degrees), building the polar grid by catching the significant coordinate pairs
-    for angle in np.arange(0, 360.01, Precision):
+    for angle in np.linspace(Min, Max, num=AdjustedTau, endpoint=True):
         # Must convert degrees to radians here
         X = np.cos(np.radians(angle))
         Y = np.sin(np.radians(angle))
@@ -42,8 +50,15 @@ def PolarGrid(Rho=1, Centroid=(0,0), Theta=2, Rings=1, Tau=1000):
             CoordList.append([Centroid[0], Centroid[1]])
             CoordList.append([AdjustedX, AdjustedY])
 
-        # Catches the final point along the perimeter of the polar grid at any angle Theta, and closes the geometry
-        elif angle % AngleTheta == 0:
+        # Catches the final point along the perimeter of the polar grid and closes the geometry.
+        elif angle == Max:
+            CoordList.append([AdjustedX, AdjustedY])
+            CoordList.append([PolarGeom[0][1][1][0],PolarGeom[0][1][1][1]]) # First point on the first radial divider.
+            CoordList.append([Centroid[0], Centroid[1]])
+            PolarGeom.append((UniqueID, CoordList))
+
+        # Catches the final point along the perimeter for each radial divider, and closes the geometry
+        elif CountIterations % TauRelativeToTheta == 0:
             CoordList.append([AdjustedX, AdjustedY])
             CoordList.append([Centroid[0], Centroid[1]])
             PolarGeom.append((UniqueID, CoordList))
@@ -58,8 +73,10 @@ def PolarGrid(Rho=1, Centroid=(0,0), Theta=2, Rings=1, Tau=1000):
         else:
             CoordList.append([AdjustedX, AdjustedY])
 
+        CountIterations += 1
+
     # Determine the number of coordinate pair entries so that our np.array is created to size
-    ArrayIndex = len(PolarGeom[0][1])
+    ArrayIndex = len(PolarGeom[0][1]) # All radial dividers will have the same number (length) of coordinate pairs
 
     # Create a np.array that stores the polar grid geometry by radial divider
     PolarGeomArray = np.asarray(PolarGeom, np.dtype([('UniqueID', np.str, 4),('XY', '<f8', (ArrayIndex, 2))]))
